fb229b2b4885ded137d414a562390f845b6e3a7e
Raiyan Zarif <razash139@gmail.com>
2020-05-21 13:26:51 +0300

    Added Local Storage

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
-$("#submit").click(function() {
-    var inputval = ($("#input").val());
-    if (inputval === "") {
-        return
-    } else {
-        var listitem = '<li class="list-group-item">' + inputval + '<button class="close text-danger">&times;</button></li>';
-        $("#list").append(listitem)
-        $("#input").val("");
+$("document").ready(function() {
+    getList()
+
+    $("#submit").click(function() {
+        var inputval = ($("#input").val());
+        if (inputval === "") {
+            return
+        } else {
+            var listitem = '<li class="list-group-item">' + inputval + '<button class="close text-danger">&times;</button></li>';
+            $("#list").append(listitem)
+            $("#input").val("");
+        }
+        saveList()
+    })
+    
+    $("ul").on('click', ".close", function() {
+        $(this).parent().animate({width: "0px", opacity: "0"}, 500, function() {
+            $(this).remove();
+        });
+        Event.stopPropagation();
+        saveList()
+    })
+    
+    $("ul").on('click', "li", function() {
+        $(this).toggleClass("list-group-item-success");
+        saveList()
+    })
+
+    function saveList() {
+        var savelistHTML = $("#list").html();
+        localStorage.setItem("listHTML", savelistHTML)
     }
-})
 
-$("ul").on('click', ".close", function() {
-    $(this).parent().animate({width: "0px", opacity: "0"}, 500, function() {
-        $(this).remove();
-    });
-    Event.stopPropagation();
-})
+    function getList() {
+        if (localStorage.getItem("listHTML")) {
+            var getlistHTML = localStorage.getItem("listHTML");
+            $("#list").html(getlistHTML);
+        }
+    }
 
-$("ul").on('click', "li", function() {
-    $(this).toggleClass("list-group-item-success");
 })
